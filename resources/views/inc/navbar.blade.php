4397803cb7fdc8e1a858cb99107ad255f98521a1
PanYmermelada <shiorishinobu@gmail.com>
2019-09-08 16:55:07 -0400

    Ajuste muestra

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="/letters">Cartas</a></li>
+                {{--<li><a href="/letters">Cartas</a></li>--}}
             </ul>
         </div>
     </div>
