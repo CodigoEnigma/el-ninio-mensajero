a0ad9888e7f86e25dd86c3afdd113b3c70f86505
PanYmermelada <shiorishinobu@gmail.com>
2019-10-12 07:20:44 -0400

    proceso con fallo

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
     require('config/db.php');
 
     if(isset($_POST['submit'])){
-        $nick = mysqli_real_escape_string($conn, $_POST['nick']);
-        $pass = mysqli_real_escape_string($conn, $_POST['pass']);
+        $nick = $_POST['nick'];
+        $pass = $_POST['pass'];
 
         if (!empty($nick)) {
             if (!filter_var($nick, FILTER_VALIDATE_EMAIL)) {
@@ -16,22 +16,26 @@
                     $errorNick = "El nombre de usuario es demasiado corto.";
                 }
             } else {
-                $errorNick = "El nombre de usuario no puede ser una direccion de correos.";
+                $errorNick = "El nombre de usuario no puede ser una direccion de correo.";
             }
         }
 
         if (!empty($pass)) {
-            if(strlen($pass) > 15){
-                $errorPass = "La contraseña es demasiado larga.";
-            }
-    
-            if(strlen($pass) < 6){
-                $errorPass = "La contraseña es demasiado corta.";
+            if (!filter_var($nick, FILTER_VALIDATE_EMAIL)) {
+                if(strlen($pass) > 15){
+                    $errorPass = "La contraseña es demasiado larga.";
+                }
+        
+                if(strlen($pass) < 6){
+                    $errorPass = "La contraseña es demasiado corta.";
+                }
+            } else {
+                $errorPass = "La contraseña no puede ser una direccion de correo.";
             }
         }
 
         if (empty($errorNick) && empty($errorPass)) {
-            $query = "SELECT * FROM administrador WHERE NOMBRE_USUARIO_ADMIN='".$nick."'AND CONTRASENIA_USUARIO='".$pass."'";
+            $query = sprintf("SELECT * FROM administrador WHERE NOMBRE_USUARIO_ADMIN='%s'AND CONTRASENIA_USUARIO='%s'", mysqli_real_escape_string($conn, $nick), mysqli_real_escape_string($conn, $pass));
             $result = mysqli_query($conn, $query);
             $usuario = mysqli_fetch_all($result, MYSQLI_ASSOC);
             if($count==1) {
@@ -56,23 +60,23 @@
         <section class="container-fluid ">
             <section class="row justify-content-center">
                 <section class="col-12 col-sm-6 col-md-3">
-                    <form class="formularioLogin">
+                    <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" class="formularioLogin">
                         <div class="form-group">
                             <label for="inpitName">Usuario</label>
                             <input type="text" id="inputName" name="nick" class="form-control" placeholder="Nombre de usuario" value="<?php if(isset($nick)) echo $nick?>" required autofocus>
                             <!--small id="emailHelp" class="form-text text-muted">No se compartira su información.</small-->
-                            <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($errorNick)) echo $errorNick ?></div>
+                            <p style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($errorNick)) echo $errorNick ?></p>
                         </div>
                         <div class="form-group">
                             <label for="inputPassword">Contraseña</label>
                             <input type="password" id="inputPassword" name="pass" class="form-control" placeholder="Password" value="<?php if(isset($pass)) echo $pass?>" required>
                             <!--small id="emailHelp" class="form-text text-muted">No comparta su contraseña.</small-->
-                            <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($errorPass)) echo $errorPass ?></div>
+                            <p style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($errorPass)) echo $errorPass ?></p>
                         </div>
                         
                         <button type="submit" class="btn btn-primary btn-block" name="submit">Enviar</button>
+                        <p style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($error)) echo $error ?></p>
                     </form>
-                    <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($error)) echo $error ?></div>
                 </section>
             </section>
         </section>   
