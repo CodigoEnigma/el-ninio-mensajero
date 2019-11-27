4ee3273d21fdde84e2e76a39a41e72cfcd6a6c8b
DenisHerrera <denis.d.herrera.r@gmail.com>
2019-11-27 19:12:14 -0400

    corrigiendo

--- editar_palabras.php
+++ editar_palabras.php
@@ -10,17 +10,6 @@
     session_start();
 include('inc/header.php'); 
  extract($_GET);
-$query = "SELECT * FROM especialidad WHERE ID_ESPECIALIDAD='$id'" ;
-require('config/db.php');
-$cons = mysqli_query($conn, $query);
-$valores = mysqli_fetch_array($cons, MYSQL_ASSOC);
-$nombre_especialidad = $valores['NOMBRE_ESPECIALIDAD'];
-$leer = $valores['LEER'];
-$responder  = $valores['RESPONDER'];
-$derivar = $valores['DERIVAR'];
-$postular =  $valores['POSTULAR'];
-$nombre = $valores['NOMBRE_ESPECIALIDAD'] ;
-mysqli_close($conn);
 ?>  
 
 <div class="container"> 
@@ -28,16 +17,53 @@ mysqli_close($conn);
     <a href="<?php echo ROOT_URL; ?>actualiza_datos_tipo_user.php?id=<?php echo $id; ?>" role = "button" style="float:left; margin:10px;">
 			 <img src="images/boton_volver.gif" class="img-fluid" alt="Responsive image" id="btn-back"  style = 'width:150px; height:50px;'>
             </a> 
-   <br><br><br>
-    <h1 align="center"><Strong>¡Cuentanos cómo estas!</Strong></h1>
-   <div style="float:center">
-       <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-			<div class="form-group" align="center">
-				<textarea name="TEXTO_CARTA" class="form-control" style = 'width:750px; height:350px;' >HOLA COMO ESTAS \n MUY BIEN Y TU</textarea><br>
-				<input type="submit" name="submit" id="enviar" value="Guardar" class="btn">
-            </div>	
-        </form>		
-   </div>   </div>
+               <div class="cabeceraSesion">
+                    <h2>AÑADIR O ELIMINAR PALABRAS CLAVE</h2>
+                </div>
+                <form class="login" method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                    <div class="input-group">
+                        <label>Escribe la palabra clave</label>
+                        <input type="text" name="palabra" placeholder="Escribe un palabra"> 
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block" name="añadir">Añadir palabra</button>
+                    <br>
+                    <button type="submit" class="btn btn-primary btn-block" name="eliminar">Eliminar palabra</button>
+                    <br>
+                </form>
+   </div>
+   
+   <?php 
+    
+    $query = "SELECT * FROM especialidad WHERE ID_ESPECIALIDAD='$id'" ;
+    require('config/db.php');
+    $cons = mysqli_query($conn, $query);
+    $valores = mysqli_fetch_array($cons, MYSQL_ASSOC);
+    $nombre_especialidad = $valores['NOMBRE_ESPECIALIDAD'];
+    mysqli_close($conn);
+
+    $carpeta_destino=$_SERVER['DOCUMENT_ROOT'].'/xampp/el-ninio-mensajero/palabras/'.$nombre_especialidad.".txt";
+    echo $carpeta_destino ;
+    $archivo = fopen("$carpeta_destino","r") or die ("PROBLEMAS AL ABRIR EL ARCHIVO TXT") ;
+        while(!feof($archivo)){
+            $traer = fgets($archivo);
+            $salto_de_linea = nl2br($traer);
+            echo $salto_de_linea ;
+        }
+    if(isset($_POST['añadir'])){
+        $texto = fopen("$carpeta_destino","a") or die ("problemas");
+        fwrite($texto, "\n");
+        fwrite($texto,$_POST['palabra']);
+        
+    }
+    
+    if(isset($_POST['eliminar'])){
+        $archivo =  fopen("$carpeta_destino","a") or die ("problemas");
+        $borrado = 0 ;
+        
+    }
+    
+    ?>
+
 
 </body>
 </html>
