864ccf995d514926c0426decac6a87d6d2e09144
DenisHerrera <denis.d.herrera.r@gmail.com>
2019-11-29 08:42:24 -0400

    verificacin de registro

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -10,11 +10,32 @@
   require('config/db.php');
 
   session_start();
-   include('inc/header.php'); ?>
+   include('inc/header.php');
+    
+    ?>
  <a href="<?php echo ROOT_URL; ?>Administrar.php" role = "button" style="float:left; margin:10px;">
 			 <img src="images/boton_volver.gif" class="img-fluid" alt="Responsive image" id="btn-back"  style = 'width:150px; height:50px;'>
             </a> 
+            
+  <div class="cabecera">
+    <h1>REGISTRAR USUARIO</h1>
+  </div>
   
+  <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" class="registrar">
+  
+  <small style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($errorR)) echo $errorR?></small>
+    
+     <div class="input-group">
+      <label>Cédula de identidad*</label>
+      <input type="text" name="ci" value="<?php if(isset($ciR)) echo $ciR?>" required autofocus>
+      <small style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if(isset($errorCiR)) echo $errorCiR ?></small>
+    </div>
+   
+  </form>
+  <?php
+    
+    
+    ?>
   
 </body>
 </html>
