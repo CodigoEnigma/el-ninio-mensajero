25e2c063194720dfe145fa6151a03d0e8ec926e3
DenisHerrera <denis.d.herrera.r@gmail.com>
2019-12-03 21:18:46 -0400

    funcional edicion de palabras, especialidad, registro y eliminacion de usuarios

--- actualizar.php
+++ actualizar.php
@@ -1,7 +1,6 @@
 <?php
 	require('config/config.php');
-	require('config/db.php');
-
+	
 	session_start();
 	if(!isset($_COOKIE)){
 		header('Location: '.ROOT_URL.'');
@@ -11,29 +10,19 @@
 		} else {
 
 			extract($_GET);
-
-				$sql="SELECT * FROM usuario WHERE ID_USUARIO=$id";
+                require('config/db.php');
+				$sql="SELECT * FROM usuario WHERE ID_USUARIO='$id'";
 				$ressql=mysqli_query($conn,$sql);
-				$row=mysqli_fetch_row ($ressql);
-
-			if (isset($_POST['editar'])) {
-				$ciR = mysqli_real_escape_string($conn, $_POST['ci']);
-				$tipoR = substr(mysqli_real_escape_string($conn, $_POST['tipo']),0,3);
-				$nombreR = mysqli_real_escape_string($conn, $_POST['nombre']);
-				$apellidoR = mysqli_real_escape_string($conn, $_POST['apellido']);
-
-			$query = "UPDATE usuario SET ID_ESPECIALIDAD='$tipoR', NOMBRE_USUARIO='$nombreR', APELLIDOS_USUARIO='$apellidoR' WHERE ID_USUARIO=$ciR";
-			if(mysqli_query($conn, $query)){
-				mysqli_close($conn);
-				header('Location: '.ROOT_URL.'Administrar.php');
-			}
-				$error = "No se pudieron guardar sus cambios.";
-			}
-		}
+				$row=mysqli_fetch_array ($ressql, MYSQL_ASSOC);
+                mysqli_close($conn);
+			
+		}//FIN ELSE
 	}
 ?>
 
-<?php include('inc/header.php'); ?>
+<?php include('inc/header.php'); 
+
+?>
 
 
 
@@ -43,7 +32,41 @@
             </a> 
   
 
-	
+	<?php
+        
+        if (isset($_POST['editar'])) {
+                require('config/db.php');
+				$ciR = mysqli_real_escape_string($conn, $_POST['ci']);
+				$tipoR = mysqli_real_escape_string($conn, $_POST['especialidad']);
+				$nombreR = mysqli_real_escape_string($conn, $_POST['nombre']);
+				$apellidoR = mysqli_real_escape_string($conn, $_POST['apellido']);
+                
+               
+            
+
+			$query = "UPDATE usuario SET ID_ESPECIALIDAD='$tipoR', NOMBRE_USUARIO='$nombreR', APELLIDOS_USUARIO='$apellidoR' WHERE ID_USUARIO=$ciR";
+           
+			if(mysqli_query($conn, $query)){
+				
+				 echo'<script type="text/javascript">
+                                    alert("Actualizacion realizada con exito");
+                                    window.location.href="'. ROOT_URL .'Administrar.php";
+                                    </script>';
+                }else{
+                        echo'<script type="text/javascript">
+                                    alert("No se pudo actualizar'.$tipoR.'");
+                                    window.location.href="'. ROOT_URL .'actualizar.php?id='.$id.'";
+                                    </script>';   }
+				
+			}
+
+    ?>
+
+
+
+
+
+
 
 	<hr class="soft">
         <div class="cabeceraSesion">
@@ -55,22 +78,41 @@
 		
 		<div class="input-group">
 		    <label>Cedula de Identidad</label> <br>
-			<input type="text" name="ci" value= "<?php echo $row[0] ?>" ><br>
+			<input type="text" name="ci" value= "<?php echo $row['ID_USUARIO'] ?>" ><br>
 		</div>
 		<div class="input-group">
         	 <label>Nombre</label>
-			 <input type="text" name="nombre" value="<?php echo $row[3];?>">
+			 <input type="text" name="nombre" value="<?php echo $row['NOMBRE_USUARIO'];?>">
 		</div>
 		
 		<div class="input-group">
 		    <label>Apellidos</label> 
-			<input type="text" name="apellido" value="<?php echo $row[4];?>">
+			<input type="text" name="apellido" value="<?php echo $row['APELLIDOS_USUARIO'];?>">
 		</div>
-        
-		<div class="input-group">
-		    <label>Especialidad</label>
-			<input type="text" name="tipo" value="<?php echo $row[7];?>">
-		</div>
+       
+        <div class="input-group">
+         <label>Especialidad: </label><br>
+          <select name="especialidad" >
+            <option value=""><?php $esp = $row['ID_ESPECIALIDAD'];
+                                require('config/db.php');
+                                $consulta_especialidad = mysqli_query($conn,"SELECT NOMBRE_ESPECIALIDAD FROM especialidad WHERE ID_ESPECIALIDAD ='$esp'");
+                                $resultado_especialidad = mysqli_fetch_array($consulta_especialidad,MYSQL_ASSOC);
+                                 mysqli_close($conn);  
+                                echo $resultado_especialidad['NOMBRE_ESPECIALIDAD'] ;
+                
+                
+                ?></option> 
+            <?php require('config/db.php');
+                $query=mysqli_query($conn,"SELECT * FROM especialidad");
+                $especs= mysqli_fetch_all($query,MYSQLI_ASSOC);
+                mysqli_close($conn);   
+                foreach($especs as $espec) : ?>
+             
+                  <option value="<?php echo $espec['ID_ESPECIALIDAD']; ?>"><?php echo $espec['NOMBRE_ESPECIALIDAD']; ?></option>
+                <?php endforeach;?>
+          </select> 
+        </div>
+		
 		    <button type="submit" name="editar" class="btn btn-primary btn-block">Guardar</button>
 			<br>
 			<a href="<?php echo ROOT_URL; ?>Administrar.php" name="cancel"  class="btn btn-primary btn-block">Cancelar</a>
